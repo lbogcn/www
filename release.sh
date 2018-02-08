16f4151236c9e8e68a0225c38c8c5b7b6fb31a45
lenbo <664967337@qq.com>
2018-02-08 23:36:44 +0800

    add release.sh

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd $(cd $(dirname $0); pwd)
+
+chmod -R 777 ./storage
+chmod -R 777 ./bootstrap/cache
+
+/usr/local/php/bin/php artisan route:cache
+/usr/local/php/bin/php artisan config:cache
+/usr/local/php/bin/php artisan optimize --force
+
+chown -R www:www $(cd `dirname $0`; pwd)
+
+/etc/init.d/php-fpm reload
