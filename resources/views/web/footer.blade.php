b8d02197d5a123f4ba843b8db119c8035ff5c153
lenbo <664967337@qq.com>
2018-02-09 22:51:05 +0800

    add baidu tongji

--- resources/views/web/footer.blade.php
+++ resources/views/web/footer.blade.php
@@ -1,3 +1,13 @@
 <footer class="copyright">
     <p>&copy; lenbo {{date('Y')}} 粤ICP备17008394号-1 Theme by Dandy's Blog</p>
 </footer>
+
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?d0754ebee2c0ba0c2983368f42b0462c";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+</script>
