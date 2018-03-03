e2bda3aca9458b3a774cee5f1be42c44f3e3f828
lenbo <664967337@qq.com>
2018-03-03 21:41:27 +0800

    add baidu zhanzhang auto push

--- resources/assets/web/js/baidu.js
+++ resources/assets/web/js/baidu.js
@@ -1,7 +1,22 @@
+// 百度统计
 let _hmt = _hmt || [];
-(function() {
+(function () {
     let hm = document.createElement("script");
     hm.src = "https://hm.baidu.com/hm.js?d0754ebee2c0ba0c2983368f42b0462c";
     let s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
+})();
+
+// 百度站长自动推送
+(function () {
+    let bp = document.createElement('script');
+    let curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    let s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
 })();
